9cd279e2c58bac4e4ae2882e3f91b7129d4e552d
Kotaro.Doi <dowing1002@gmail.com>
2025-11-15 11:07:46 +0900

    動作モード切替機能の追加

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,27 @@
 #include	"main.h"
 #include	"morning_evening.h"
 #include	"yukari.h"
+#include	"fwmode.h"
 
 
 /* 関数宣言 */
 static void main_Connect_NintendoSw_init();
-static void Yukari_Use_SkillA();
-static void pushRStick();
-
 
 /* setup */
 void setup(){
 	/* 起動時、「持ち方/順番を変える」になっている前提 */
 	main_Connect_NintendoSw_init();
+}
+
+/* loop */
+void loop(){
+	if(FWSETTING_NUM > INVALID_SETTING){	/* 複数の機能が選択されている場合なにもしない */
+		/* NOP */
+	}else if(BENCHSKIP == FUNC_ON){			/* ベンチで朝晩スキップすることでの厳選 */
+		morning_to_evening();
+	}else if(YUKARI_TOURNAMENT == FUNC_ON){	/* ゆかりトーナメントでの金策レベリング */
+		Yukari_tournament();
+	}
 }
 
 /* 起動時の処理 */
@@ -22,12 +31,4 @@ static void main_Connect_NintendoSw_init()
 	pushButton(Button::L, DELAY_500MS, 5);			/* Switchがにマイコンを認識させる処理(500msごとに2回L押下) */
 	/* コントローラ認識済み */
 	pushButton(Button::HOME, DELAY_1S*2,2);			/* homeに戻る、ゲームに戻る */
-}
-
-
-
-/* loop */
-void loop(){
-	//morning_to_evening();
-	Yukari_tournament();
 }
